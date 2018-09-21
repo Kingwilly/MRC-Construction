568f7dfa7161cc779fcae5a9055aea74240a7d19
Bedrock123 <info@builtbykingwilly.com>
2018-09-21 11:16:16 -0400

    Finished footer desktop and mobile

--- src/components/Common/Footer.js
+++ src/components/Common/Footer.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Row, Col } from "antd";
 import Logo from "../../assets/images/Common/logo_small.svg";
+import NBC from "../../assets/images/Social/nbc.svg";
 
 class Footer extends Component {
   // Global Bottom Footer
@@ -43,8 +44,61 @@ class Footer extends Component {
             <Col xs={{ span: 24 }} sm={{ span: 8 }}>
               <div className="footer-block">
                 <b className="footer-title">FOLLOW US ON SOCIAL MEDIA & NBC</b>
-                <div className="footer-inner-block">
-                  <p />
+                <div className="footer-inner-block inner-block-tight">
+                  <Row>
+                    <Col xs={{ span: 6 }} sm={{ span: 12 }} lg={{ span: 6 }}>
+                      <a
+                        href="https://www.nbc.com/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        <img
+                          src={NBC}
+                          alt="NBC"
+                          className="social-media-icon"
+                        />
+                      </a>
+                    </Col>
+                    <Col xs={{ span: 6 }} sm={{ span: 12 }} lg={{ span: 6 }}>
+                      <a
+                        href="https://www.nbc.com/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        <img
+                          src={NBC}
+                          alt="NBC"
+                          className="social-media-icon"
+                        />
+                      </a>
+                    </Col>
+                    <Col xs={{ span: 6 }} sm={{ span: 12 }} lg={{ span: 6 }}>
+                      <a
+                        href="https://www.nbc.com/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        <img
+                          src={NBC}
+                          alt="NBC"
+                          className="social-media-icon"
+                        />
+                      </a>
+                    </Col>
+                    <Col xs={{ span: 6 }} sm={{ span: 12 }} lg={{ span: 6 }}>
+                      <a
+                        href="https://www.nbc.com/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        <img
+                          src={NBC}
+                          alt="NBC"
+                          className="social-media-icon"
+                        />
+                      </a>
+                    </Col>
+                  </Row>
                 </div>
               </div>
             </Col>
