b294ef0a137131f2626fc8f8ddb5f5b21206fb70
Bedrock123 <info@builtbykingwilly.com>
2018-11-25 14:09:11 -0500

    Addd in form

--- src/containers/Contact.js
+++ src/containers/Contact.js
@@ -20,6 +20,22 @@ class Contact extends Component {
             <InfoBar />
           </div>
           <ContactForm />
+          <form name="contact" method="POST" netlify>
+            <p>
+              <label>
+                Email: <input type="text" name="name" />
+              </label>
+            </p>
+            <p>
+              <label>
+                Message: <textarea name="message" />
+              </label>
+            </p>
+            <div data-netlify-recaptcha />
+            <p>
+              <button type="submit">Send</button>
+            </p>
+          </form>
         </div>
       </DocumentTitle>
     );
