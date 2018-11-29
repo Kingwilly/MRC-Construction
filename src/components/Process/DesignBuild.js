a0078f6537a2ce0f41d52e19cc693d03a34cc8ba
Bedrock123 <info@builtbykingwilly.com>
2018-11-29 12:51:23 -0500

    Added in small fixed

--- src/components/Process/DesignBuild.js
+++ src/components/Process/DesignBuild.js
@@ -20,7 +20,8 @@ class DesignBuild extends Component {
               Our Project Mangers become an extension of our client’s family
               during renovations.
             </p>
-            <br />
+          </Col>
+          <Col xs={{ span: 24 }} md={{ span: 12 }} style={{ padding: "9px" }}>
             <p>
               {" "}
               Aside from being on site every day, they hold weekly meetings to
@@ -29,26 +30,6 @@ class DesignBuild extends Component {
               changes are clearly communicated. For the MRC team, managing the
               building process is not just about a superior end result, but
               about maximizing client satisfaction.
-            </p>
-          </Col>
-          <Col xs={{ span: 24 }} md={{ span: 12 }} style={{ padding: "9px" }}>
-            <p>
-              Ellie Mroz is Mike's partner in life (aka, wife) and work. She
-              leads her own boutique interior design company, Ellie Mroz Design
-              , in a shared downtown Westfield headquarters with MRC. MRC
-              clients are not obliged to use EMD's interior design services,
-              however, this is an option many clients take advantage of for a
-              full-service construction approach, given the importance of final
-              interior design to truly complete the project.
-            </p>
-            <br />
-            <p>
-              Ellie and her team take pride in designing homes that feel
-              authentic and personalized, with an aesthetic that has been
-              described as Modern Traditional. It’s a bright, approachable,
-              fresh, and organic take on the traditional style that defines
-              Westfield and the surrounding towns. EMD’s designs have been
-              described as touchable, livable, layered, laid-back and luxe.
             </p>
           </Col>
         </Row>
